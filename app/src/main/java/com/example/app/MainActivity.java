b382e516234e1d64f98fddbeff3502f9692a654b
JIN26 <Angelmantilla26@gmail.com>
2020-03-08 19:57:17 -0500

    diseño base de la primera ventana

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,27 +3,36 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText user,password;
+    private RadioButton hombre,mujer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        user = (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.password);
+        //hombre = (RadioButton) findViewById(R.id.rb);
+        Toast.makeText(this, "OnResume", Toast.LENGTH_LONG).show();
         // La actividad se ha creado
     }
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_LONG).show();
         // La actividad está a punto de hacerse visible.
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_LONG).show();
         // La actividad se ha vuelto visible (ahora se "reanuda").
     }
     @Override
@@ -35,13 +44,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_LONG).show();
         // La actividad ya no es visible (ahora está "detenida")
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_LONG).show();
         // La actividad está a punto de ser destruida.
+    }
+    public void guardarDatos(View view){
+        String dato = user.getText().toString()+" "+password.getText().toString();
+        Toast.makeText(this, dato, Toast.LENGTH_LONG).show();
     }
 }
